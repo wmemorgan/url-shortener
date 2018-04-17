c464748d35d3df36280201dbe3d2677c050d1af4
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-17 21:23:59 +0800

    Add connectivity to MongoDB On branch dev Your branch is up to date with 'origin/dev'. Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 require('dotenv').load();
 
 const express = require('express'),
-  app = express(),
+  mongodb = require('mongodb'),
   path = require('path'),
-  port = process.env.PORT || 3000;
+  MongoClient = mongodb.MongoClient,
+  url = process.env.DBCONNECT,
+  port = process.env.PORT || 3000,
+  app = express();
+
+MongoClient.connect(url, (err, conn) => {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+    conn.close();
+  }
+})
 
 app.listen(port, () => {
   console.log("Server is listening on port:", port);

8985a91bc8bc0ae279739320dca96c5ceb5a73ea
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-17 21:51:33 +0800

    Add module to generate URL ids

--- server.js
+++ server.js
@@ -3,19 +3,22 @@ require('dotenv').load();
 const express = require('express'),
   mongodb = require('mongodb'),
   path = require('path'),
+  shortid = require('shortid'),
   MongoClient = mongodb.MongoClient,
   url = process.env.DBCONNECT,
   port = process.env.PORT || 3000,
   app = express();
 
-MongoClient.connect(url, (err, conn) => {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to', url);
-    conn.close();
-  }
-})
+// MongoClient.connect(url, (err, conn) => {
+//   if (err) {
+//     console.log('Unable to connect to the mongoDB server. Error:', err);
+//   } else {
+//     console.log('Connection established to', url);
+//     conn.close();
+//   }
+// })
+
+console.log(shortid.generate());
 
 app.listen(port, () => {
   console.log("Server is listening on port:", port);

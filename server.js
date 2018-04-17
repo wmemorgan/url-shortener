adaea76f7b37ccb095b5d1a31c9fdce001cf9037
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-17 21:57:38 +0800

    Add module to validate URL strings On branch dev  Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express'),
   mongodb = require('mongodb'),
   path = require('path'),
   shortid = require('shortid'),
+  validURL = require('valid-url'),
   MongoClient = mongodb.MongoClient,
   url = process.env.DBCONNECT,
   port = process.env.PORT || 3000,
@@ -18,7 +19,16 @@ const express = require('express'),
 //   }
 // })
 
-console.log(shortid.generate());
+// console.log(shortid.generate());
+
+//Valid URLs
+let myURL = '1';
+
+if (validURL.isUri(myURL)) {
+  console.log('Looks like an URI');
+} else {
+  console.log('Not a URI');
+}
 
 app.listen(port, () => {
   console.log("Server is listening on port:", port);
